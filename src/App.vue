21ef83c73038967c0dd61f77940299ee292cc82a
Chris Xu <chriskxu.tictac@gmail.com>
2023-05-25 04:46:37 +0000

    added custom layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
-import Header from './components/Header.vue';
-import Editor from './components/Editor.vue';
 </script>
 
 <template>
-  <Header />
-  <Editor />
+  <nav class="navbar navbar-expand-lg bg-secondary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Todl Playground</a>
+    </div>
+  </nav>
+
+  <div class="container-fluid bg-dark flex-grow-1 overflow-auto">
+    <div class="row align-items-start mt-2">
+      <div class="col">
+        <textarea class="w-100"></textarea>
+      </div>
+      <div class="col">
+        <div class="row-fluid container">
+          <textarea class="w-100 h-50"></textarea>
+        </div>
+        <div class="row-fluid container">
+          <textarea class="w-100 h-50"></textarea>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
